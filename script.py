5251fac78192b85d859896d40fe78936af48da88
Trevor Patch <trevor.patch@gmail.com>
2016-02-18 23:10:24 -0600

    Outlining needed steps

--- script.py
+++ script.py
@@ -1,3 +1,21 @@
 import RPi.GPIO as GPIO
+import datetime
+
+print(datetime.datetime.now().time())
+#datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
 
 GPIO.setmode(GPIO.BCM)
+# Pins to set up
+# 1. Power (on/off)
+# 2. Data
+
+
+# Get alarm time from server (different script)
+
+# Store time in case wifi connection is lost (different script)
+
+# Check if current time matches alarm time
+
+# If yes, trigger light sequence
+
+# Detect button during light sequence to disable light
